43496e2b487e81d648186d517bb90657abb9ec42
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-08-07 17:55:53 -0400

    Configured JMS.

---
+++ src/main/java/pdtg/lsmscbeerordersrvc/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package pdtg.lsmscbeerordersrvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by Diego T. 07-08-2022
+ */
+@Configuration
+public class JmsConfig {
+
+    public static final String BEER_ORDER_SVC_QUEUE= "beer-order-svc-queue";
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
